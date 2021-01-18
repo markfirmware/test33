2ea76c0e94e364885397b005d7c86407fc513534
markfirmware <mark.kendrat@gmail.com>
2021-01-18 00:53:43 +0000

    update issue

--- search.sh
+++ search.sh
@@ -22,5 +22,3 @@ gq https://api.github.com/graphql -H "Authorization: bearer $GITHUB_TOKEN" \
     }
   }
 }' > search.json
-
-cat search.json | jq ".data.search.repos | .[] | .repo | {login:.owner.login, name:.name}"
